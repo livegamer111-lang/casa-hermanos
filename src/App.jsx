48bb9cebf7f900f19023b1b8fdce882612726398
Casa <livegamer.111@gmail.com>
2026-05-23 14:45:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -883,6 +883,14 @@ export default function App() {
           </div>
         </div>
       </section>
+      <a
+  href="https://wa.me/31639315783"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 w-16 h-16 rounded-full flex items-center justify-center shadow-2xl text-3xl"
+>
+  💬
+</a>
     </div>
   );
 }
